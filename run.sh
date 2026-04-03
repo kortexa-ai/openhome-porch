a0bb5b5bd6d997d516d1c94680241a3b6688e757
Franci Penov <francip@gmail.com>
2026-04-02 22:43:37 -0700

    fixing local dev

--- run.sh
+++ run.sh
@@ -2,10 +2,8 @@
 set -e
 cd "$(dirname "$0")"
 
-if [ "$1" = "--dev" ]; then
-    ./build.sh --debug
-else
-    ./build.sh
-fi
+# run.sh always builds debug with --skip-window
+# Window is launched from source dir at runtime in dev mode
+./build.sh --debug --skip-window
 
 exec Porch.app/Contents/MacOS/Porch
